846ebada9d6efe60401d81a5f39ac540be58ad83
herbgarden <vksxkwl65@gmail.com>
2020-07-17 13:05:12 +0900

    auto

---
+++ chrome_add.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo '[google-chrome]' > /etc/yum.repos.d/google-chrome.repo
+echo 'name=google-chrome ' >> /etc/yum.repos.d/google-chrome.repo
+echo 'baseurl=http://dl.google.com/linux/chrome/rpm/stable/$basearch' >> /etc/yum.repos.d/google-chrome.repo
+echo 'enabled=1' >> /etc/yum.repos.d/google-chrome.repo
+echo 'gpgcheck=1' >> /etc/yum.repos.d/google-chrome.repo
+echo 'gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub' >> /etc/yum.repos.d/google-chrome.repo
+
+yum install -y google-chrome-stable
+
+#sed -n '49s/"$@"/"$@" --no-sandbox --user-data-diri/'
+
+
